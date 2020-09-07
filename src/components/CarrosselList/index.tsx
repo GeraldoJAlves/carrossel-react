ace17c7c7a3924ac89bfe1a23624b163668b874e
Geraldo Alves <junior.alves.2007@gmail.com>
2020-09-07 10:53:22 -0300

    commit initial

---
+++ src/components/CarrosselList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CarrosselList: React.FC = () => {
+  return (
+    <Container>
+      <h1>CarrosselList</h1>
+    </Container>
+  );
+};
+
+export default CarrosselList;
